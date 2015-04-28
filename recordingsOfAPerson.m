0ebbb08444a8afbd6ffa3f01c86238c6f411c883
budel <daniel.budelmann@student.jade-hs.de>
2015-04-28 16:13:01 +0200

    added empty function recordingsOfAPerson

---
+++ recordingsOfAPerson.m
@@ -0,0 +1,41 @@
+function [recordings] = recordingsOfAPerson(name)
+% function to show the recordings of a person
+% Usage [recordings] = recordingOfAPerson(name)
+% Input Parameter:
+%	 name: 		 cell array with strings of the names of the person,
+%                e.g. marc0, fvmh0, etc.
+% Output Parameter:
+%	 recordings: 	 cell array with the recordings of that person. Can be
+%                    played back with soundsc(recordings{x}).
+%------------------------------------------------------------------------ 
+% Example: [recordings] = recordingsOfAPerson("marc0")
+
+% Author: Daniel Budelmann and Sebastian Voges (c) TGM @ Jade Hochschule applied licence see EOF 
+% Version History:
+% Ver. 0.01 initial create (empty) 28-Apr-2015  Initials (eg. DB and SV)
+
+%------------Your function implementation here--------------------------- 
+
+
+
+
+
+%--------------------Licence ---------------------------------------------
+% Copyright (c) <2015> Daniel Budelmann and Sebastian Voges
+% Jade University of Applied Sciences 
+% Permission is hereby granted, free of charge, to any person obtaining 
+% a copy of this software and associated documentation files 
+% (the "Software"), to deal in the Software without restriction, including 
+% without limitation the rights to use, copy, modify, merge, publish, 
+% distribute, sublicense, and/or sell copies of the Software, and to
+% permit persons to whom the Software is furnished to do so, subject
+% to the following conditions:
+% The above copyright notice and this permission notice shall be included 
+% in all copies or substantial portions of the Software.
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
